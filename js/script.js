86bcb98a8b0c23dd312a1e43d29200cbe1f6684b
mcclendokj11 <118197174+KyleM021199@users.noreply.github.com>
2023-03-24 22:40:40 -0500

    Added js and html framework

--- js/script.js
+++ js/script.js
@@ -1,2 +1,42 @@
+var beginButton = document.querySelector(".begin-button");
+
+
+//array for questions and answers
+var questions = [];
+var answers = [];
+
+function Timer(){
+
+}
+function setScore(){
+
+}
+function getScore(){
+
+}
+
+function startQuiz(){
+
+}
+
+function gameEnd(){
+
+}
+
+function renderAnswers(){
+
+}
+function renderQuestions(){
+
+}
+
+//reset the quiz via back button 
+var backButton = document.querySelector(".back-button");
+
+function resetQuiz(){
+
+}
+
+backButton.addEventListener("click", resetQuiz);
 
 
