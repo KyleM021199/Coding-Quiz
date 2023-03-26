6de8867c36920d94d0f157c07697c2282ef7e03b
mcclendokj11 <118197174+KyleM021199@users.noreply.github.com>
2023-03-25 20:26:30 -0500

    Added js work to functions: getScore, setTimer, setScore, startQuiz and init

--- js/script.js
+++ js/script.js
@@ -1,25 +1,64 @@
+//querySelectors
 var beginButton = document.querySelector(".begin-button");
+var timerEl = document.querySelector(".timer");
+// variables
+var secondCount;
+var timeLoss;
+var quizEnded = false;
+var finalScore = 0;
 
 
 //array for questions and answers
 var questions = [];
 var answers = [];
 
-function Timer(){
+function startQuiz(){
+    quizEnded = false;
+    secondCount = 10;
+    beginButton.disable = true;
+    renderQuestions();
+    renderAnswers();
+    setTimer();
 
 }
+function init(){
+    getScore();
+}
+//sets the timer    
+function setTimer(){
+
+    var timer = setInterval(function(){
+        secondCount--;
+        timerEl.textContent = secondCount;
+        if(secondCount >= 0){
+            if(quizEnded && secondCount > 0){
+                setScore();
+                clearInterval(timer);
+                quizEnd();
+            }
+        }
+
+
+
+    }, 1000);
+}
 function setScore(){
+    timerEl.textContent = finalScore;
+    localStorage.setItem("endScore", finalScore);
 
 }
 function getScore(){
-
+    var storedScore = localStorage.getItem("endScore");
+    if (storedScore === null){
+        finalScore = 0;
+    } else {
+        finalScore = storedScore;
+    }
+   timerEl.textContent = finalScore;
 }
 
-function startQuiz(){
 
-}
-
-function gameEnd(){
+function quizEnd(){
 
 }
 
@@ -29,6 +68,9 @@ function renderAnswers(){
 function renderQuestions(){
 
 }
+
+beginButton.addEventListener("click", startQuiz);
+init();
 
 //reset the quiz via back button 
 var backButton = document.querySelector(".back-button");
