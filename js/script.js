4ad01748b99a46a1852221f04d4e8387b88e1cd5
mcclendokj11 <118197174+KyleM021199@users.noreply.github.com>
2023-03-30 16:03:09 -0500

    Added style.css and fixed/added to js and html files

--- js/script.js
+++ js/script.js
@@ -1,67 +1,75 @@
 //querySelectors
+var pageOne = document.querySelector("#page-1");
+var pageTwo = document.querySelector("#page-2");
+var pageThree = document.querySelector("#page-3");
 var beginButton = document.querySelector(".begin-button");
 var timerEl = document.querySelector(".timer");
 var score = document.querySelector(".score");
 var questionDiv = document.querySelector(".question-prompt");
 var answersLi = document.querySelector(".answer-list");
+var userNameEl = document.querySelector("#user-name");
 
 // variables
 var secondCount;
 var questionNum = 1;
-var timeLoss = secondCount -= 10;
+//var timeLoss = secondCount -= 10;
 var quizEnded = false;
 var finalScore = 0;
 var timer;
-var isRight = false;
 var nextCounter = 0;
+var correctAnswerCounter= 1;
 
 //array for questions/answers
 var quizQuestions = [{
     question: "question1",
-    answer : ["answer1","answer2","answer3"],
-    rightAnswer: "answer1"}, 
+    answer : ["answer1-1","answer2-1","answer3-1"],
+    rightAnswer: "answer1-1"}, 
 {
     question: "question2",
-    answer : ["answer1","answer2","answer3"],
-    rightAnswer: "answer2"},
+    answer : ["answer1-2","answer2-2","answer3-2"],
+    rightAnswer: "answer2-2"},
 
  {
     question: "question3",
-    answer : ["answer1","answer2","answer3"],
-    rightAnswer: "answer2"},
+    answer : ["answer1-3","answer2-3","answer3-3"],
+    rightAnswer: "answer2-3"},
  {
     question: "question4",
-    answer : ["answer1","answer2","answer3"],
-    rightAnswer: "answer3"},
+    answer : ["answer1-4","answer2-4","answer3-4"],
+    rightAnswer: "answer3-4"},
 ];
 function init(){
     getScore();
 }
-function checkRight(string){
-    console.log(string);
-    console.log(quizQuestions[questionNum - 1].rightAnswer)
-    if(string === quizQuestions[questionNum - 1].rightAnswer){
+function checkRight(clicked){
+    
+    for(var i=0; i < 4; i++){
+    console.log(clicked);
+    if(clicked === quizQuestions[questionNum-1].rightAnswer){
+        
         console.log("is true");
+        correctAnswerCounter++;
         return true;
     } else {
     console.log("is false");
-        timeLoss;
+    secondCount -= 10;
+        correctAnswerCounter++;
         return false;
+        }
+
+
     }
-
-
 }
-
-/* function checkWrong(answer){
-    if(rightAnswer !== answer){
-        isRight = false;
-        timeLoss;
-    }
-} */
 //starts the quiz
 function startQuiz(){
+
+    pageOne.style.display = "none";
+    pageTwo.style.display = "block";
+    pageThree.style.display = "none";
+    if(secondCount <=0){
+        secondCount = 50;
+    }
     quizEnded = false;
-    secondCount = 50;
     beginButton.disabled = true;
     renderQuestions(0);
     setTimer();
@@ -73,15 +81,20 @@ function setTimer(){
 
      timer = setInterval(function(){
         secondCount--;
-        timerEl.textContent = secondCount;
+        timerEl.textContent = "Seconds left: "+ secondCount;
         if(secondCount >= 0){
 
             if(quizEnded && secondCount >= 0){
                 setScore();
                 clearInterval(timer);
                 quizEnd();
+                checkRight();
             }
             
+        }else if(secondCount === 1){
+            secondCount--;
+        timerEl.textContent = "Second left: "+ secondCount;
+
         }
         if(secondCount === 0){
             setScore();
@@ -89,29 +102,44 @@ function setTimer(){
             quizEnd();
         }
 
+
     }, 1000);
 }
 function setScore(){
-    finalScore = secondCount;
-    score.textContent = finalScore;
-    localStorage.setItem("endScore", finalScore);
+    //timerEl.textContent = finalScore;
+    score.textContent = secondCount;
+    localStorage.setItem("endScore", secondCount);
     
 
 }
 function getScore(){
     var storedScore = localStorage.getItem("endScore");
     if (storedScore === null){
-        finalScore = 0;
+        secondCount = 0;
     } else {
-        finalScore = storedScore;
+        secondCount = storedScore;
     }
-   timerEl.textContent = finalScore;
-
+   score.textContent = secondCount;
+   //timerEl.textContent = finalScore;
 }
 
 
 function quizEnd(){
+    pageTwo.style.display = "none";
+    pageThree.style.display = "block";
 //user types in preferred name and saved to the local storage
+/* userNameEl.addEventListener('keydown', function (event) {
+    // Access value of pressed key with key property
+    var key = event.key.toLowerCase();
+    var alphabetNumericCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789 '.split(
+      ''
+    );
+    if (alphabetNumericCharacters.includes(key)) {
+        for (var i = 0; i < elements.length; i++) {
+          elements[i].textContent += event.key;
+        }
+      }
+    }); */
 
 quizEnded = true;
 beginButton.disabled = false;
@@ -139,10 +167,11 @@ function nextQuestion(){
 
 
 
-
+pageOne.style.display = "block";
+pageTwo.style.display = "none";
+pageThree.style.display = "none";
 beginButton.addEventListener("click", startQuiz);
 init();
-checkRight();
 // lets the answers have click elements
 document.querySelectorAll('ul.answer-list li').forEach((item) => {
     item.addEventListener('click', (event) => {
@@ -164,9 +193,11 @@ document.querySelectorAll('ul.answer-list li').forEach((item) => {
 var backButton = document.querySelector(".back-button");
 
 function resetQuiz(){
-    
+    secondCount = 0;
+    // console.log(finalScore);
+    // console.log(secondCount);
     setScore();
-    finalScore = 0;
+   
 }
 
 backButton.addEventListener("click", resetQuiz);
